e08d045e8bb3b3eb49144b7d3c5e63b3c48fcb0a
Oleksandr Khomenko <okhomenko@gmail.com>
2015-09-10 13:53:29 -0500

    Removed trailing commas

--- protractor.conf.js
+++ protractor.conf.js
@@ -10,13 +10,13 @@ exports.config = {
         //},
         {
             browserName: 'chrome'
-        },
+        }
     ],
 
     suites: {
         smokes: 'tests/smoketests/*.js',
         app: 'tests/*Spec.js'
-    },
+    }
 
     // Direct connect to Chrome, other browsers can work only through webdriver (Selenium)
     //directConnect: true
